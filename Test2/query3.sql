8e73a04fd0ab034ad8764ed130f4241d2114b4e6
skorobov9 <62547397+skorobov9@users.noreply.github.com>
2022-03-23 16:00:33 +0300

    Update query3.sql

--- Test2/query3.sql
+++ Test2/query3.sql
@@ -3,5 +3,5 @@ join stack.Orders on Customers.row_id = customer_id
 join stack.OrderItems on Orders.row_id = OrderItems.order_id
 where YEAR(Orders.registered_at)=2020
 group by stack.Customers.name
-having ( COUNT(order_id) = 
-( select COUNT(stack.OrderItems.name) from stack.OrderItems where  name='Кассовый аппарат'))
+having ( COUNT(distinct order_id) = 
+( select COUNT(distinct case when OrderItems.name='РљР°СЃСЃРѕРІС‹Р№ Р°РїРїР°СЂР°С‚' then order_id end)))
